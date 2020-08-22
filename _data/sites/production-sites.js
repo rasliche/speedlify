e4ac61da43bb5910f3bd36f2c3246ea7b6ddd665
Eric Raslich <rasliche@gmail.com>
2020-08-22 12:26:52 -0400

    Eric's production sites to test

---
+++ _data/sites/production-sites.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: "Eric's Sites", // optional, falls back to object key
+	description: "Production sites made by Eric.",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "run"
+	},
+	urls: [
+		"https://www.keywestgroundparrot.com/",
+		"https://www.kpimodoro.com/",
+	]
+};
