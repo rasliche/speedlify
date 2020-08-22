3ff59e6672ad686b28f5ff7098bacc83fbf8a453
Eric Raslich <rasliche@gmail.com>
2020-08-22 12:26:42 -0400

    Eric's personal site pages to test

---
+++ _data/sites/ericraslich.js
@@ -0,0 +1,30 @@
+module.exports = {
+	name: "ericraslich.com", // optional, falls back to object key
+	description: "Eric’s Personal web site",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
+	},
+	urls: [
+		"https://ericraslich.com/",
+		"https://ericraslich.com/projects/",
+		"https://ericraslich.com/posts/",
+		"https://ericraslich.com/uses/",
+		"https://ericraslich.com/garden/",
+		"https://ericraslich.com/hire/",
+		"https://ericraslich.com/cv/",
+		// "Popular" Projects
+		"https://ericraslich.com/projects/couch-to-5k/",
+		// Popular Posts
+		"https://ericraslich.com/posts/2020-05-10-ethereum-fast-sync-raspberry-pi-4/",
+		"https://ericraslich.com/posts/2020-04-14-something-about-a-gecko/",
+		"https://ericraslich.com/posts/2020-03-15-blue-hole-key-deer/"
+	]
+};
